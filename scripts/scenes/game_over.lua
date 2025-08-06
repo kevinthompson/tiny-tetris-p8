fe9b06a10f1ceb546e1cace866f0a49cfb16ba21
Kevin Thompson <email@kevinthompson.info>
2025-08-05 22:20:55 -0700

    add game over screen

--- scripts/scenes/game_over.lua
+++ scripts/scenes/game_over.lua
@@ -1,5 +1,17 @@
 game_over = scene:extend({
   init = function(_ENV)
+    local grid = game.grid
+
+    async(function()
+      for y = #grid, 1, -1 do
+        for x = 1, #grid[1] do
+          if grid[y][x] != 0 then
+            grid[y][x] = 25
+          end
+        end
+        wait(5)
+      end
+    end)
   end,
 
   update = function(_ENV)
@@ -12,6 +24,15 @@ game_over = scene:extend({
 
   draw = function(_ENV)
     game:draw()
-    printc("game over scene", 28, 7)
+    rectfill(2, 12, 47, 20, 0)
+    ? "game over", 7, 14, 7
+
+    -- prompt
+    rectfill(2, 42, 47, 50, 0)
+
+    if flr(t() * 2) % 2 == 0 then
+      spr(10, 11, 43)
+      ? "again", 20, 44, 7
+    end
   end,
 })
